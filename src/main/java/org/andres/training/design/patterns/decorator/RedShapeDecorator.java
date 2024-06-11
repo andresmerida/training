0522c7b619716673caaf0cfd9ecda1191cc1998d
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2024-06-11 17:35:30 -0400

    Adding decorator pattern

---
+++ src/main/java/org/andres/training/design/patterns/decorator/RedShapeDecorator.java
@@ -0,0 +1,18 @@
+package org.andres.training.design.patterns.decorator;
+
+public class RedShapeDecorator extends ShapeDecorator {
+    public RedShapeDecorator(Shape decoratorShape) {
+        super(decoratorShape);
+    }
+
+    @Override
+    public void draw() {
+        decoratorShape.draw();
+        setRedBorder(decoratorShape);
+    }
+
+    private void setRedBorder(Shape decoratorShape) {
+        System.out.println("Border color: Red");
+    }
+
+}
