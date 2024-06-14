58ca1d4863ad38343d8fef28ae1e25ec80db8a3d
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2024-06-14 09:39:30 -0400

    Adding code challenge 'missing numbers solved'

--- src/main/java/org/andres/training/hackerrank/missing_numbers/MissingNumbers.java
+++ src/main/java/org/andres/training/hackerrank/missing_numbers/MissingNumbers.java
@@ -2,9 +2,10 @@ package org.andres.training.hackerrank.missing_numbers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
+import java.util.Set;
 
 /**
  * Given two arrays of integers, find which elements in the second array are missing from the first array.
@@ -21,27 +22,23 @@ import java.util.Objects;
  */
 public class MissingNumbers {
     public static void main(String[] args) {
-        //List<Integer> arr = Arrays.asList(7,2,5,3,5,3);
-        //List<Integer> brr = Arrays.asList(7,2,5,4,6,3,5,3);
-        List<Integer> arr = Arrays.asList(203,204,205,206,207,208,203,204,205,206);
-        List<Integer> brr = Arrays.asList(203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204);
+        List<Integer> arr = Arrays.asList(7,2,5,3,5,3);
+        List<Integer> brr = Arrays.asList(7,2,5,4,6,3,5,3);
+        // List<Integer> arr = Arrays.asList(203,204,205,206,207,208,203,204,205,206);
+        // List<Integer> brr = Arrays.asList(203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204);
         System.out.println(missingNumbers(arr, brr));
     }
 
     private static List<Integer> missingNumbers(List<Integer> arr, List<Integer> brr) {
         List<Integer> missingNumbers = new ArrayList<>();
-        arr.sort(Comparator.comparingInt(Integer::intValue));
-        brr.sort(Comparator.comparingInt(Integer::intValue));
+        Set<Integer> uniqueNumbers = new HashSet<>(brr);
+        int frequenceA, frequenceB;
 
-        System.out.println(arr);
-        System.out.println(brr);
-
-        int indexArrayMissing = 0;
-        for (int originalValue : brr) {
-            if (originalValue == arr.get(indexArrayMissing)) {
-                indexArrayMissing++;
-            } else {
-                missingNumbers.add(originalValue);
+        for (Integer uniqueNumber : uniqueNumbers) {
+            frequenceA = Collections.frequency(arr, uniqueNumber);
+            frequenceB = Collections.frequency(brr, uniqueNumber);
+            if (frequenceA != frequenceB) {
+                missingNumbers.add(uniqueNumber);
             }
         }
 
