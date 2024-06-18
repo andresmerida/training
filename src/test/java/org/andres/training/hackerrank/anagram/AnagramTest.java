329a2ad67d6439f3244f7552b0ad204d5549fae8
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2024-06-17 23:50:48 -0400

    adding is anagram solution

---
+++ src/test/java/org/andres/training/hackerrank/anagram/AnagramTest.java
@@ -0,0 +1,26 @@
+package org.andres.training.hackerrank.anagram;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnagramTest {
+
+    @Test
+    public void givenAnagram_thenReturnZero() {
+        String anagramInput = "xyyx";
+        int actualMinValue = Anagram.minChangeToAnagram(anagramInput);
+        int expected = 0;
+        assertEquals(actualMinValue, expected);
+    }
+
+    @Test void givenMinOneToBeAnagram_thenReturnOne() {
+        String input = "xaxbbbxx";
+        assertEquals(1, Anagram.minChangeToAnagram(input));
+    }
+
+    @Test void givenDifferentLength_thenReturnOneNegative() {
+        String input = "abc";
+        assertEquals(-1, Anagram.minChangeToAnagram(input));
+    }
+}
