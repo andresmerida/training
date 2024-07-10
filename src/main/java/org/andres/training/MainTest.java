ed14fad47a2d8b693ed3c67dbde5c0b10acb55ab
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2024-07-10 15:09:57 -0400

    Adding out

--- src/main/java/org/andres/training/MainTest.java
+++ src/main/java/org/andres/training/MainTest.java
@@ -9,9 +9,14 @@ public class MainTest {
         linkedList.add(1);
         linkedList.add(2);
         linkedList.add(3);
+        linkedList.add(5);
+        linkedList.add(3, 4);
         System.out.println(linkedList);
         String s1 = "hello";
         s1.concat(" world!");
         System.out.println(s1);
+
+        System.out.println(100 + 100 + "KeepOnCoding");
+        System.out.println("KeepOnCoding" + 100 + 100);
     }
 }
