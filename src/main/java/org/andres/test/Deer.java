a4e1f707edb216f79d4cc489e7c75e231919c5a7
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2025-05-20 09:42:48 -0400

    adding changes

---
+++ src/main/java/org/andres/test/Deer.java
@@ -0,0 +1,22 @@
+package org.andres.test;
+
+import java.util.stream.Stream;
+
+public class Deer {
+    public Deer() {
+        System.out.println("Deer");
+    }
+
+    public Deer(int age) {
+        System.out.println("DeerAge");
+    }
+
+    private boolean hasHorns() {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.iterate("", s -> s + "1");
+        System.out.println(stream.limit(2).map(s -> s + "2"));
+    }
+}
---
+++ src/main/java/org/andres/test/Item.java
@@ -0,0 +1,13 @@
+package org.andres.test;
+
+public class Item {
+    public int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
