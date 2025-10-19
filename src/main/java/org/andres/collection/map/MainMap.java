ad6f5b3714f8754b23feef863f622e0229740d7b
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2025-10-18 23:47:37 -0400

    adding litle change

---
+++ src/main/java/org/andres/collection/map/MainMap.java
@@ -0,0 +1,10 @@
+package org.andres.collection.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainMap {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+    }
+}
