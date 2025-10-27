c941e1a4d4ce42abe34ce4f177c2af9d072a62b8
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2025-10-27 17:55:18 -0400

    Adding two sum codeleed

--- src/main/java/org/andres/leetcode/two_sum/TwoSum.java
+++ src/main/java/org/andres/leetcode/two_sum/TwoSum.java
@@ -3,19 +3,59 @@ package org.andres.leetcode.two_sum;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Example 1:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * <p/>
+ * Example 2:
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * <p/>
+ * Example 3:
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ */
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        if (nums.length < 2 || nums.length > Math.pow(10, 4)) {
-            throw new IllegalArgumentException("Invalid input");
-        }
-        Map<Integer, Integer> pairIdx = new HashMap<>();
+        checkInputs(nums, target);
+        int[] res = new int[2];
 
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            if (pairIdx.containsKey(target - num)) {
-                return new int[] { i, pairIdx.get(target - num)};
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
             }
-            pairIdx.put(num, i);
+        }
+
+        return null;
+    }
+
+    private void checkInputs(int[] nums, int target) {
+        if (nums == null || nums.length < 2 || nums.length > Math.pow(10, 4)) {
+            throw new IllegalArgumentException("Invalid array input!");
+        }
+
+        if (target < -Math.pow(10, 9) || target > Math.pow(10, 9)) {
+            throw new IllegalArgumentException("Invalid target!");
+        }
+    }
+
+    public int[]  twoSumBestSolution(int[] nums, int target) {
+        checkInputs(nums, target);
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
         }
 
         return null;
--- src/test/java/org/andres/leetcode/two_sum/TwoSumTest.java
+++ src/test/java/org/andres/leetcode/two_sum/TwoSumTest.java
@@ -5,31 +5,80 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TwoSumTest {
-    TwoSum twoSum = new TwoSum();
+    private final TwoSum twoSum = new TwoSum();
 
     @Test
     void twoSumTest() {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
-        int[] result = twoSum.twoSum(nums, target);
+        int[] expected = new int[]{0, 1};
 
-        assertNotNull(result);
-        assertEquals(1, result[0]);
-        assertEquals(0, result[1]);
+        assertArrayEquals(expected, twoSum.twoSum(nums, target));
     }
 
     @Test
-    void twoSumTest_IllegalArgumentException() {
-        int[] nums = {1};
-        int target = 100;
-        assertThrows(IllegalArgumentException.class, () -> twoSum.twoSum(nums, target));
+    void twoSumTest_threeValues() {
+        int[] nums = {3,2,4};
+        int target = 6;
+        int[] expected = {1, 2};
+
+        assertArrayEquals(expected, twoSum.twoSum(nums, target));
     }
 
     @Test
-    void twoSumTest_NoSolution() {
-        int[] nums = {1, 2, 3};
-        int target = 100;
-        int[] result = twoSum.twoSum(nums, target);
-        assertNull(result);
+    void twoSumTest_twoValues() {
+        int[] nums = {3,3};
+        int target = 6;
+        int[] expected = {0, 1};
+
+        assertArrayEquals(expected, twoSum.twoSum(nums, target));
+    }
+
+    @Test
+    void twoSumTest_IllegalArgumentException_nullArray() {
+        assertThrows(IllegalArgumentException.class, () -> twoSum.twoSum(null, 0));
+    }
+
+    @Test
+    void twoSumTest_IllegalArgumentException_targetError() {
+        assertThrows(IllegalArgumentException.class, () -> twoSum.twoSum(new int[]{1,2,3,4,5}, (int)Math.pow(10, 10)));
+    }
+
+    // test best solution
+    @Test
+    void twoSumBestSolutionTest() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] expected = new int[]{0, 1};
+
+        assertArrayEquals(expected, twoSum.twoSumBestSolution(nums, target));
+    }
+
+    @Test
+    void twoSumBestSolution_threeValues() {
+        int[] nums = {3,2,4};
+        int target = 6;
+        int[] expected = {1, 2};
+
+        assertArrayEquals(expected, twoSum.twoSumBestSolution(nums, target));
+    }
+
+    @Test
+    void twoSumBestSolution_twoValues() {
+        int[] nums = {3,3};
+        int target = 6;
+        int[] expected = {0, 1};
+
+        assertArrayEquals(expected, twoSum.twoSumBestSolution(nums, target));
+    }
+
+    @Test
+    void twoSumBestSolution_IllegalArgumentException_nullArray() {
+        assertThrows(IllegalArgumentException.class, () -> twoSum.twoSumBestSolution(null, 0));
+    }
+
+    @Test
+    void twoSumBestSolution_IllegalArgumentException_targetError() {
+        assertThrows(IllegalArgumentException.class, () -> twoSum.twoSumBestSolution(new int[]{1,2,3,4,5}, (int)Math.pow(10, 10)));
     }
 }
