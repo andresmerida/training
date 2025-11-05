1e1affacd940d7b86fcb2119c800c8a8aacb534e
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2025-11-05 09:06:22 -0400

    Adding two string anagrams

---
+++ src/test/java/org/andres/leetcode/valid_anagram/ValidAnagramTest.java
@@ -0,0 +1,75 @@
+package org.andres.leetcode.valid_anagram;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidAnagramTest {
+    private final ValidAnagram validAnagram = new ValidAnagram();
+
+    @Test
+    void isAnagram_trueExample1() {
+        String s = "listen";
+        String t = "silent";
+
+        assertTrue(validAnagram.isAnagram(s, t));
+    }
+
+    @Test
+    void isAnagram_trueExample2() {
+        String s = "heart";
+        String t = "earth";
+
+        assertTrue(validAnagram.isAnagram(s, t));
+    }
+
+    @Test
+    void isAnagram_falseExample() {
+        String s = "andres";
+        String t = "merida";
+
+        assertFalse(validAnagram.isAnagram(s, t));
+    }
+
+    @Test
+    void isAnagram_throwIllegalArgumentException_null() {
+        assertThrows(IllegalArgumentException.class, () -> validAnagram.isAnagram(null, "test"));
+    }
+
+    @Test
+    void isAnagram_throwIllegalArgumentException_empty() {
+        assertThrows(IllegalArgumentException.class, () -> validAnagram.isAnagram("", ""));
+    }
+
+    @Test
+    void isAnagram_throwIllegalArgumentException_Uppercases() {
+        String s = "Listen";
+        String t = "Silent";
+        assertThrows(IllegalArgumentException.class, () -> validAnagram.isAnagram(s, t));
+    }
+
+    // Second solution
+    @Test
+    void isAnagramSolutionTwo_trueExample1() {
+        String s = "listen";
+        String t = "silent";
+
+        assertTrue(validAnagram.isAnagram(s, t));
+    }
+
+    @Test
+    void isAnagramSolutionTwo_trueExample2() {
+        String s = "heart";
+        String t = "earth";
+
+        assertTrue(validAnagram.isAnagram(s, t));
+    }
+
+    @Test
+    void isAnagramSolutionTwo_falseExample() {
+        String s = "andres";
+        String t = "merida";
+
+        assertFalse(validAnagram.isAnagram(s, t));
+    }
+}
