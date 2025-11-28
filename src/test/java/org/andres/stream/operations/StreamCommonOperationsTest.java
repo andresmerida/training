a4278f553c8e48b1fe85d92d63ae1eb6795654a4
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2025-11-28 16:47:40 -0400

    Adding common operations with Stream

--- src/test/java/org/andres/stream/operations/StreamCommonOperationsTest.java
+++ src/test/java/org/andres/stream/operations/StreamCommonOperationsTest.java
@@ -39,4 +39,101 @@ class StreamCommonOperationsTest {
 
         assertEquals(expected, streamCommonOperations.convertListToMapWithSizes(list1));
     }
+
+    @Test
+    void reduceOperationsTest() {
+        List<Integer> input = Arrays.asList(1, 2, 3, 4);
+        Map<String, Object> expected = Map.of("sum", 10,
+                "product", 24,
+                "max", 4,
+                "min", 1,
+                "concat", "1234");
+
+        assertEquals(expected, streamCommonOperations.reduceOperations(input));
+    }
+
+    @Test
+    void filterNoPrimeNumbersTest() {
+        List<Integer> input = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> expected = List.of(2, 3, 5, 7);
+
+        assertEquals(expected, streamCommonOperations.filterNoPrimeNumbers(input));
+    }
+
+    @Test
+    void getPartitionStringsByLengthTest() {
+        List<String> input = List.of("apple", "banana", "cherry", "date", "fig", "grapefruit", "kiwi");
+        Map<String, List<String>> mapExpected = Map.of("short", List.of("date", "fig", "kiwi"), "medium", List.of("apple", "banana", "cherry", "grapefruit"));
+
+        assertEquals(mapExpected, streamCommonOperations.getPartitionStringsByLength(input));
+    }
+
+    @Test
+    void findRepeatedCharactersTest() {
+        String input = "programming";
+        List<Character> expected = List.of('r', 'g', 'm');
+
+        assertEquals(expected, streamCommonOperations.findRepeatedCharacters(input));
+    }
+
+    @Test
+    void findFirstRecurrentElementTest() {
+        int[] input = {1, 2, 3, 4, 5, 3, 6, 7};
+        int expected = 3;
+        assertEquals(expected, streamCommonOperations.findFirstRecurrentElement(input));
+    }
+
+    @Test
+    void findFirstRecurrentElementTest_noRepeated() {
+        int[] input = {1, 2, 3, 4, 5, 6, 7};
+        int expected = -1;
+        assertEquals(expected, streamCommonOperations.findFirstRecurrentElement(input));
+    }
+
+    @Test
+    void findFirstRecurrentElementTest_null() {
+        assertThrows(IllegalArgumentException.class, () -> streamCommonOperations.findFirstRecurrentElement(null));
+    }
+
+    @Test
+    void longestCommonPrefix() {
+        String[] input = {"flower", "flow", "flight"};
+        String expected = "fl";
+
+        assertEquals(expected, streamCommonOperations.longestCommonPrefix(input));
+    }
+
+    @Test
+    void longestCommonPrefix_ExampleTwo() {
+        String[] input = {"flower", "flow", "flor", "flot"};
+        String expected = "flo";
+
+        assertEquals(expected, streamCommonOperations.longestCommonPrefix(input));
+    }
+
+    @Test
+    void longestCommonPrefix_Throw() {
+        assertThrows(IllegalArgumentException.class, () -> streamCommonOperations.longestCommonPrefix(null));
+    }
+
+    @Test
+    void longestCommonPostfix() {
+        String[] input = {"Andrew", "Randrew", "Mastrew"};
+        String expected = "rew";
+
+        assertEquals(expected, streamCommonOperations.longestCommonPostfix(input));
+    }
+
+    @Test
+    void longestCommonPostfix_ExampleFour() {
+        String[] input = {"Andrew", "Randrew", "Mastrew", "Mathew"};
+        String expected = "ew";
+
+        assertEquals(expected, streamCommonOperations.longestCommonPostfix(input));
+    }
+
+    @Test
+    void longestCommonPostfix_Throw() {
+        assertThrows(IllegalArgumentException.class, () -> streamCommonOperations.longestCommonPostfix(null));
+    }
 }
